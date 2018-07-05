fc07ee91c594cafc58a9b42df71a6cfc01c7d57a
unknown <obinnacodes@gmail.com>
2018-07-05 12:55:21 +0100

    obinna added his initial edits

---
+++ src/category.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Category extends Component{
+    render(){
+        <div >
+            <ul>
+                
+            </ul>
+        </div>
+    }
+}
+
+export default Category 
